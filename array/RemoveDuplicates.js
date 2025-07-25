70b0504e9fea3f52211fef96e4150e82bff195c1
Manish Pendse <46042390+devManishPendse@users.noreply.github.com>
2025-07-25 18:01:26 +0530

    Update RemoveDuplicates.js

--- array/RemoveDuplicates.js
+++ array/RemoveDuplicates.js
@@ -2,22 +2,19 @@ function removeDuplicates(arr) {
 
 
     let newArray = [];
-    for (let i = 0, x = 0; i < arr.length; i++) {
-        if (arr[i] === arr[i + 1]) {
-        }
-        else {
-            arr[x] = arr[i]
+    let x = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > arr[x]) {
             x++;
-            
-            // newArray.push(arr[i])
+            arr[x] = arr[i];
         }
     }
-    console.log(arr);
+    return x + 1;
 
 }
 
 
-removeDuplicates([0, 0, 1, 2, 2, 3, 3, 4, 5])
+console.log(removeDuplicates([0, 0, 1, 2, 2, 3, 3, 4, 5]))
 
 //time complexcity 
 //O(n^2)
