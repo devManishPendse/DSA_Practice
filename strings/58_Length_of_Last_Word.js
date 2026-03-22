a12e07a5b6368cc1cbc96e93b889a2e8419937fe
Manish Pendse <46042390+devManishPendse@users.noreply.github.com>
2026-03-22 22:22:21 +0530

    Update 58_Length_of_Last_Word.js

--- strings/58_Length_of_Last_Word.js
+++ strings/58_Length_of_Last_Word.js
@@ -1,1 +1,22 @@
 
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function (s) {
+    counter = 0;
+    for (i = s.length-1; i >= 0; i--) {
+        
+        if (s[i] === ' ' && counter === 0) {
+            continue;
+        } else if (s[i] === ' ' && counter > 0) {
+            break;
+        }
+        else {
+            counter++;
+        }
+    }
+
+
+    return counter;
+};
