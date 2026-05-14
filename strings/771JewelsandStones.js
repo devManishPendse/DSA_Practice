ac02bcb6c5e581c40a82ad55cdbe7b742c435e7c
Manish Pendse <46042390+devManishPendse@users.noreply.github.com>
2026-05-14 10:27:22 +0530

    Update 771JewelsandStones.js

--- strings/771JewelsandStones.js
+++ strings/771JewelsandStones.js
@@ -14,3 +14,26 @@ var numJewelsInStones = function(jewels, stones) {
     }
     return count;
 };
+
+
+//optimized
+
+/**
+ * @param {string} jewels
+ * @param {string} stones
+ * @return {number}
+ */
+var numJewelsInStones = function (jewels, stones) {
+    let mySet = new Set();
+    let count = 0;
+    for (let i = 0; i < jewels.length; i++) {
+        mySet.add(jewels[i])
+    }
+
+    for (let j = 0; j < stones.length; j++) {
+        if (mySet.has(stones[j])) {
+            count++;
+        }
+    }
+    return count;
+};
