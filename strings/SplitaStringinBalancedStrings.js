b9938b611d0072e19ea1ae8c75e0ac300a6ab80f
Manish Pendse <46042390+devManishPendse@users.noreply.github.com>
2026-08-06 17:20:58 +0530

    Create SplitaStringinBalancedStrings.js

---
+++ strings/SplitaStringinBalancedStrings.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var balancedStringSplit = function (s) {
+    let lcount = 0, rcount = 0,count = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] == 'L') {
+            lcount = lcount + 1;
+
+        }
+        if (s[i] == 'R') {
+            rcount = rcount + 1;
+
+        }
+        if(lcount === rcount){
+            count++;
+            lcount = rcount = 0;
+        }
+
+    }
+    console.log(count);
+    return count;
+};
